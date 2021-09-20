49210c997131d0838f7fbe1551bb56ad8b8476da
Guilherme Mateus Michalach <guilhermemichalach@gmail.com>
2021-09-20 15:53:45 -0300

    Scanner feito, só faltando o uso do clipboard

---
+++ app/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+// Fazer essa implementação posteriormente
+import Clipboard from '@react-native-clipboard/clipboard';
+
+export default function App() {
+  const [hasPerm, setHasPerm] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Não escaneado ainda.')
+
+  const askForCameraPerm = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPerm(status === 'granted');
+    })()
+  }
+
+  // Request Camera Permission
+
+  useEffect(() => {
+    askForCameraPerm();
+  }, []);
+
+  // O que acontece ao escanear
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Tipo: ' + type + '\nDados: ' + data)
+  };
+
+  // Checar permissões e retornar telas com base nelas
+
+  if (hasPerm === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>)
+  }
+  if (hasPerm === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button title={'Permitir câmera'} onPress={() => askForCameraPerm()} />
+      </View>)
+  }
+
+  // Retorno da View com permissão concedida
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} 
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button title={'Escanear novamente?'} onPress={() => setScanned(false)} color='tomato' />}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+    marginTop: 5
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato'
+  }
+});
